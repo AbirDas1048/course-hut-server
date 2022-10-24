2c724ada74b519821d3dec5710ddfc5b042ee8f9
Abir Das <abirdas422@gmail.com>
2022-10-25 01:11:59 +0600

    added cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Course Hut Server.')
