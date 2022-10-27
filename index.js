fe3f4685b966834370a68accb8d2dc38a02faae8
Abir Das <abirdas422@gmail.com>
2022-10-27 09:45:11 +0600

    Sample courses data added

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const port = process.env.PORT || 5000
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Course Hut Server.')
 })
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const course = courses.filter(c => c.id === id);
+    res.send(course);
+});
 
 app.listen(port, () => {
     console.log(`Course Hut Server listening on port ${port}`)
